62b0ba3733a88bfa801beaef43f8a514f3d749e3
sdi2121 <159460782+sdi2121@users.noreply.github.com>
2025-11-30 17:31:15 -0500

    Updating project structure

--- apps/pumpkin-api/obj/Debug/net10.0/pumpkin-api.AssemblyInfo.cs
+++ apps/pumpkin-api/obj/Debug/net10.0/pumpkin-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pumpkin-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76cb48f0e4f5d353d9f78c51f8c6adefa2b8ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41c4796f64478e711cdbd806a3d8ee305134454")]
 [assembly: System.Reflection.AssemblyProductAttribute("pumpkin-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pumpkin-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
