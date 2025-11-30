f41c4796f64478e711cdbd806a3d8ee305134454
sdi2121 <159460782+sdi2121@users.noreply.github.com>
2025-11-30 15:46:13 -0500

    Move test project

--- apps/pumpkin-net-models/obj/Debug/net10.0/pumpkin-net-models.AssemblyInfo.cs
+++ apps/pumpkin-net-models/obj/Debug/net10.0/pumpkin-net-models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pumpkin-net-models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7022ce44c0c7f2bc3ec7885f975e054a34a0f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76cb48f0e4f5d353d9f78c51f8c6adefa2b8ef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("pumpkin-net-models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pumpkin-net-models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
